51dd3fe3dbba450100194e0f21f77c598b5b9b22
dontbesoupset <dontbesoupset@gmail.com>
2026-03-31 09:50:50 +0300

    ЛР 8: jQuery и эффекты. Подключена библиотека jQuery, добавлена подсветка постов при наведении (highlight-post.js), реализован эффект увеличения логотипа при наведении через animate()

---
+++ lab8/blog/articles/static/js/highlight-post.js
@@ -0,0 +1,24 @@
+// highlight-post.js - подсветка постов при наведении
+
+$(document).ready(function(){
+    $('.one-post').hover(
+        function(event){
+            // Навели курсор
+            $(event.currentTarget).find('.one-post-shadow').animate({opacity: '0.2'}, 300);
+        },
+        function(event){
+            // Убрали курсор
+            $(event.currentTarget).find('.one-post-shadow').animate({opacity: '0'}, 300);
+        }
+    );
+});
+
+// Эффект увеличения логотипа при наведении
+$('.header img').hover(
+    function(){
+        $(this).animate({width: '338px'}, 300); 
+    },
+    function(){
+        $(this).animate({width: '318px'}, 300);
+    }
+);
---
+++ lab8/blog/blog/urls.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'articles.views.archive', name='archive'),
+    url(r'^article/(?P<article_id>\d+)/$', 'articles.views.get_article', name='get_article'),
+    
+    url(r'^article/new/$', 'articles.views.create_post', name='create_post'),
+    
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'articles.views.login_user', name='login'),
+    url(r'^register/$', 'articles.views.register_user', name='register'),
+)
